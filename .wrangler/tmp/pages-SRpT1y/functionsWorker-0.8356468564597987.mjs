b82683c481e4fbf7e1fb73c71b273a2d8cabb72a
wliuy <wliuy666@gmail.com>
2026-04-08 21:11:08 +0800

    feat: Sarah MUSIC 10.1.10 (增加服务端 PASSWORD 变量认证，恢复 1:1 源码保真)

--- .wrangler/tmp/pages-SRpT1y/functionsWorker-0.8356468564597987.mjs
+++ .wrangler/tmp/pages-SRpT1y/functionsWorker-0.8356468564597987.mjs
@@ -715,7 +715,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// ../.wrangler/tmp/bundle-kSPIPk/middleware-insertion-facade.js
+// ../.wrangler/tmp/bundle-x6oWSQ/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -747,7 +747,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// ../.wrangler/tmp/bundle-kSPIPk/middleware-loader.entry.ts
+// ../.wrangler/tmp/bundle-x6oWSQ/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
